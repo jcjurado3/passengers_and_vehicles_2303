1167c099ef8d2f6caae7a5dd7ea326fad6d48b63
Jimmy Jurado <jurado140@gmail.com>
2023-04-05 11:17:58 -0400

    Adds and tests the adult? method.

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,10 +1,18 @@
 class Passenger
 
-  attr_reader :name, :age
+  attr_reader :name, :age, :adult
 
   def initialize(player_info)
     @name = player_info["name"]
     @age = player_info["age"]
+    @adult = adult
+  end
+
+  def adult?
+      if @age >= 18 
+        @adult = true
+      else @adult = false 
+      end
   end
 
 end
