53ede523b141cdc4fd35ae3fb5344f5d80a0bb10
Jimmy Jurado <jurado140@gmail.com>
2023-04-05 11:12:29 -0400

    Fixes passenger class and updates hash keys to string. Tested attributes correctly.

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -3,8 +3,8 @@ class Passenger
   attr_reader :name, :age
 
   def initialize(player_info)
-    @name = player_info[:name]
-    @age = player_info[:age]
+    @name = player_info["name"]
+    @age = player_info["age"]
   end
 
 end
