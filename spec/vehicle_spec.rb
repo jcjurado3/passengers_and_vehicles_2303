76540e80731a7452178e596c5e9b488ea5ff1671
Jimmy Jurado <jurado140@gmail.com>
2023-04-05 11:28:52 -0400

    Add tests for vehicle object and attributes.

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,21 @@
+require './lib/vehicle'
+require './lib/passenger'
+require 'rspec'
+
+RSpec.describe Vehicle do
+  describe '#initialize' do
+    it 'exists' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      expect(vehicle).to be_a(Vehicle)
+    end
+    it 'has attributes' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      expect(vehicle.year).to eq("2001")
+      expect(vehicle.make).to eq("Honda")
+      expect(vehicle.model.to eq("Civic")
+
+    end
+  end
+end
