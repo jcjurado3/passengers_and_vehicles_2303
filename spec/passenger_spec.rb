e7aa537ae0399a45f1dfd2594d0d246542bce0da
Jimmy Jurado <jurado140@gmail.com>
2023-04-05 11:11:20 -0400

    Adds tests for attributes.

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe Passenger do
       expect(charlie).to be_a(Passenger)
       expect(taylor).to be_a(Passenger)
     end
+
+    it 'has attributes' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})  
+
+      expect(charlie.name).to eq("Charlie")
+      expect(charlie.age).to eq(18)
+    end
   end
 
 end
